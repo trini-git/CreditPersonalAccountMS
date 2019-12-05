ae83020f685a090d5b7dc27a91bab36945180626
U-USERSAD\ptrinida <ptrinida@LIM-JTZXMF2.usersad.everis.int>
2019-12-04 23:02:14 -0500

    Sixth Microservice

---
+++ src/main/java/com/creditpersonalaccount/CreditPersonalAccountMsApplication.java
@@ -0,0 +1,13 @@
+package com.creditpersonalaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditPersonalAccountMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditPersonalAccountMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/creditpersonalaccount/CreditPersonalAccountMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creditpersonalaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditPersonalAccountMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
